2836ff6172585336246d1fde1d076dad40996e20
optik1 <bearden86@gmail.com>
2024-06-17 13:59:41 -0400

    added header/footer to contact page, html structure of contact form, and js functionality/script tags

--- success.js
+++ success.js
@@ -3,3 +3,5 @@
 setTimeout(() => {
     window.location.href = window.location.origin
   }, 5000)
+
+//   this is a timeout that will close the "success" landing page and return to index.html after 5000ms
