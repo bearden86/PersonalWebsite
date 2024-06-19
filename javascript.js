4460df707474ea4c61b9574b5a715cd4fce8e26d
optik1 <bearden86@gmail.com>
2024-06-18 20:09:54 -0400

    added styling for contact form page

--- javascript.js
+++ javascript.js
@@ -29,7 +29,8 @@ const form = document.querySelector('#form')
 const submitButton = document.querySelector('#submit')
 
 form.addEventListener('submit', (e) => {
-  submitButton.disabled = true
-  e.preventDefault()
-  window.location.href = window.location.origin + '/success.html'
+  submitButton.disabled = true;
+  e.preventDefault();
+  e.stopPropagation();
+  window.location.href = window.location.origin + '/success.html';
 })
